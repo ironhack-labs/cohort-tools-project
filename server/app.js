0f1c1c132145e040fe8e2e8a38e3b5f6a4ca943f
Rony Laforest <rrlaforest@gmail.com>
2023-10-30 14:42:23 +0100

    Day1 dev changes - set up client and server

--- server/app.js
+++ server/app.js
@@ -7,10 +7,8 @@ const PORT = 5005;
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -21,14 +19,29 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
+
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+const cohorts = require(__dirname + "/cohorts.json");
+
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
+
+const students = require(__dirname + "/students.json");
+
+app.get("/api/students", (req, res) => {
+  res.json(students);
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("404");
+});
 
 // START SERVER
 app.listen(PORT, () => {
