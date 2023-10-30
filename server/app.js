5f191ddb42afe5ff19b68d5c0b539df54e85930b
Fabien Lacordelle <falaco972@icloud.com>
2023-10-30 15:19:00 +0100

    day1

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -18,6 +19,13 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
