2aa61764549771a4f2792544c88856f602c6f030
romain.carissan2@gmail.com <romain.carissan2@gmail.com>
2023-10-31 17:44:49 +0100

    second group lab done

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const cors = require("cors");
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const PORT = 5005;
 
 // STATIC DATA
@@ -9,6 +11,91 @@ const PORT = 5005;
 // ...
 const DataCohorts = require("./cohorts.json");
 const DataStudents = require("./students.json");
+
+const cohortSchema = new Schema({
+  inProgress: { type: Boolean, default: false },
+  cohortSlug: { type: String, required: true, unique: true },
+  cohortName: { type: String, required: true },
+  program: {
+    type: String,
+    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
+  },
+  format: {
+    type: String,
+    enum: ["Full Time", "Part Time"],
+  },
+  campus: {
+    type: String,
+    enum: [
+      "Madrid",
+      "Barcelona",
+      "Miami",
+      "Paris",
+      "Berlin",
+      "Amsterdam",
+      "Lisbon",
+      "Remote",
+    ],
+  },
+  startDate: { type: Date, default: "current date" },
+  endDate: { type: Date },
+  programManager: {
+    type: String,
+    required: true,
+  },
+  leadTeacher: {
+    type: String,
+    required: true,
+  },
+  totalHours: { type: Number, default: 360 },
+});
+
+const studentSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  linkedinUrl: { type: String, default: "" },
+
+  languages: {
+    type: [String],
+    enum: [
+      "English",
+      "Spanish",
+      "French",
+      "German",
+      "Portuguese",
+      "Dutch",
+      "Other",
+    ],
+  },
+  program: {
+    type: String,
+    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
+  },
+  background: { type: String, default: "" },
+  image: { type: String, default: `https://i.imgur.com/r8bo8u7.png` },
+
+  projects: Array,
+  cohort: { type: mongoose.SchemaTypes.ObjectId, ref: "Student" },
+});
+
+const Cohort = mongoose.model("Cohort", cohortSchema);
+const Student = mongoose.model("Student", studentSchema);
+
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
@@ -22,6 +109,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohorts-tools-api")
+  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
+
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
@@ -30,14 +122,19 @@ app.get("/docs", (req, res) => {
 });
 
 app.get("/api/cohorts", (req, res) => {
-  res.json(DataCohorts);
+  Cohort.find({});
+  res.json(); //we have to retrive the data from the api inside the brackets
 });
 
 app.get("/api/students", (req, res) => {
-  res.json(DataStudents);
+  Student.find({});
+  res.json();
 });
 
 // START SERVER
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+module.exports = Cohort;
+module.exports = Student;
