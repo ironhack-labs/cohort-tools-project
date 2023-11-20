793187813cd17a9fe843767e0f294ce3f7e542a0
David Lucca <david.v.lucca@gmail.com>
2023-11-20 16:44:44 +0000

    Routes for docs, api/cohorts and api/students made and working

--- server/app.js
+++ server/app.js
@@ -5,7 +5,8 @@ const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
+const cohorts = require('./cohorts.json')
+const students = require('./students.json')
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
@@ -29,6 +30,13 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
+
+app.get("/api/students", (req, res) => {
+  res.json(students);
+});
 
 // START SERVER
 app.listen(PORT, () => {
