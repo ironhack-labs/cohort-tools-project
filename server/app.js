65cde4165127c51b631fc245d0d8f18cde38b2b4
David Lucca <david.v.lucca@gmail.com>
2023-11-21 16:12:52 +0000

    Day 2 done, created a database on mongoBD and created schemas and models on app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const PORT = 5005;
 
 // STATIC DATA
@@ -44,3 +46,110 @@ app.get("/api/students", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+// MONGOOSE
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api/cohorts")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
+
+
+  const cohortsSchema = new Schema({
+    cohortSlug: {
+      type: String,
+      required: true,
+    },
+    cohortName: {
+      type: String,
+      required: true,
+    },
+    program: {
+      type: String,
+      enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
+    },
+    format: {
+      type: String,
+      enum: ["Full Time", "Part Time"],
+    },
+    campus:{
+      type: String,
+      enum: ["Madrid", "Barcelona", "Miami", "Paris", "Berlin", "Amsterdam", "Lisbon", "Remote"],
+    },
+    startDate: {
+      type: Date,
+      default: Date.now(),
+    },
+    endDate: {
+      type: Date,
+    },
+    inProgress:{
+      type: Boolean,
+      default: false,
+    },
+    programManager:{
+      type: String,
+      required: true,
+    },
+    leadTeacher:{
+      type: String,
+      required: true,
+    },
+    totalHours:{
+      type: Number,
+      default: 360,
+    },
+  });
+
+  const Cohort = mongoose.model("Cohort", cohortsSchema);
+
+
+
+
+const studentSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  linkedinUrl: {
+    type: String,
+    default: '',
+  },
+  languages: {
+    type: [String],
+    enum: ["English", "Spanish", "French", "German", "Portuguese", "Dutch", "Other"],
+  },
+  program: {
+    type: String,
+    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
+  },
+  background: {
+    type: String,
+    default: '',
+  },
+  image: {
+    type: String,
+    default: 'https://i.imgur.com/r8bo8u7.png',
+  },
+  cohort: {
+    type: Schema.Types.ObjectId,
+    ref: 'Cohort',
+  },
+  projects: {
+    type: Array,
+  },
+});
+
+const Student = mongoose.model('Student', studentSchema);
