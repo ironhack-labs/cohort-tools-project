fe5d2ee2b0ec49dcc5d148ca159bce14a80b0354
pomerama <marpom@gmail.com>
2023-11-28 17:43:41 +0100

    installed mongoose. setup api points for cohorts and students. TO FIX: Student Schema ObjectId for Cohort

--- server/app.js
+++ server/app.js
@@ -2,44 +2,177 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const cors = require("cors")
+const mongoose = require("mongoose");
+
+const Cohort = require("./models/Cohort")
+const Student = require("./models/Student")
+
 const PORT = 5005;
 
+// Setup mongoose connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
+
+
 // STATIC DATA
-// Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
 const cohorts = require("./cohorts.json");
 const students = require("./students.json")
+
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
 
 // MIDDLEWARE
-// Research Team - Set up CORS middleware here:
-// ...
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors())
-// app.use(cors({origin: []}))
+
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
-// Devs Team - Start working on the routes here:
-// ...
+
+// ------ Api endpoints for Cohorts ------ 
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
 app.get("/api/cohorts", (req, res) => {
-  res.json(cohorts);
+  Cohort.find({})
+    .then((cohorts) => {
+      console.log("Retrieve cohorts: ", cohorts);
+      res.status(200).json(cohorts);
+    })
+    .catch((error) => {
+      res.status(500).send({ message: "Failed getting cohorts: " + error });
+    })
+})
+
+app.get("/api/cohorts/:cohortId", (req, res) => {
+  Cohort.findById(req.params.cohortId)
+    .then((cohort) => {
+      res.status(200).json(cohort);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Failed getting a cohort: " + error });
+    })
+});
+
+app.post("/api/cohorts", (req, res) => {
+  Cohort.create({
+    inProgress: req.body.inProgress,
+    cohortSlug: req.body.cohortSlug,
+    cohortName: req.body.cohortName,
+    program: req.body.program,
+    campus: req.body.campus,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    programManager: req.body.programManager,
+    leadTeacher: req.body.leadTeacher,
+    totalHours: req.body.totalHours
+  })
+    .then((createdCohort) => {
+      res.status(200).json(createdCohort)
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error while creating a cohort: " + error })
+    })
 
 })
 
+app.put("/api/cohorts/:cohortId", (req, res) => {
+  Cohort.findByIdAndUpdate(req.params.cohortId, req.body, { new: true })
+    .then((updatedCohort) => {
+      res.status(200).json(updatedCohort);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Failed updating a cohort: " + error })
+    })
+})
+
+app.delete("/api/cohorts/:cohortId", (req, res) => {
+  Cohort.findByIdAndDelete(req.params.cohortId)
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Failed deleting a cohort: " + error })
+    })
+})
+
+
+
+// ------ Api endpoints for Students ------ 
 app.get("/api/students", (req, res) => {
-  res.json(students);
-
+  Student.find({})
+    .then((students) => {
+      console.log("Retrieve students " + students);
+      res.status(200).json(students)
+    })
+    .catch((error) => {
+      res.status(500).send({ message: "Failed getting students: " + error })
+    })
 })
+
+app.get("/api/students/:studentId", (req, res) => {
+  Student.findById(req.params.studentId)
+    .then((student) => {
+      res.status(200).json(student);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Failed getting a student: " + error });
+    })
+});
+
+app.post("/api/students", (req, res) => {
+  Student.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    phone: req.body.phone,
+    linkedinUrl: req.body.linkedinUrl,
+    languages: req.body.languages,
+    program: req.body.program,
+    background: req.body.background,
+    image: req.body.image,
+    projects: req.body.projects,
+    // cohort: ObjectId,
+  })
+    .then((createdStudent) => {
+      res.status(200).json(createdStudent)
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error while creating a student: " + error })
+    })
+})
+
+app.put("/api/students/:studentId", (req, res) => {
+  Student.findByIdAndUpdate(req.params.studentId, req.body, { new: true })
+    .then((updatedStudent) => {
+      res.status(200).json(updatedStudent);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Failed updating a student: " + error })
+    })
+})
+
+app.delete("/api/students/:studentId", (req, res) => {
+  Student.findByIdAndDelete(req.params.studentId)
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Failed deleting a student: " + error })
+    })
+})
+
+
+// TODO: after the Student Schema has correct ObjectId for Cohort
+// api/students/cohort/:cohortId
+
 
 // START SERVER
 app.listen(PORT, () => {
