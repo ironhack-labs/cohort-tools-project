66f9661b2c72c7ed11baeaa7cc7d8f6faa07ce29
Wellington Abreu <wellington.abreu@gmail.com>
2024-01-02 18:29:39 -0400

    Completed day 1

--- server/app.js
+++ server/app.js
@@ -1,34 +1,47 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
 
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
-
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
-// ...
+
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
-// ...
+
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/cohorts", (req, res, next) => {
+  return res.status(200).json(cohorts);
+});
+
+app.get("/api/cohorts/:cohortId", (req, res, next) => {
+  const { cohortId } = req.params;
+  return res.status(200).json(cohorts.filter((cohort) => cohort._id === Number(cohortId)));
+});
+
+app.get("/api/students", (req, res, next) => {
+  return res.status(200).json(students);
+})
 
 // START SERVER
 app.listen(PORT, () => {
