f9944a7d32c6d0768553b75df44fe6261a955e27
Michael Vu <vu.michaeltien@gmail.com>
2024-01-03 08:55:09 -0500

    Cleaned up app.js for easier read

--- server/app.js
+++ server/app.js
@@ -5,30 +5,34 @@ const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
-
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
-
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
-// ...
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
-// ...
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
+
+app.get("/api/students", (req, res) => {
+  res.json(students);
+});
 
 // START SERVER
 app.listen(PORT, () => {
