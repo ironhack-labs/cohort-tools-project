91ef10ccefc15b0e7c5290a4e3f44709b440172b
michelmnds <m1tchofc@gmail.com>
2024-01-16 14:28:01 +0000

    merging

--- server/app.js
+++ server/app.js
@@ -29,8 +29,8 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(cors());
+app.use(cookieParser());
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
