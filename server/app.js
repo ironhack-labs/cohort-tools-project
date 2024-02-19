9f2152fb7c6c0175e13e89aac5e678e831380e76
TinyjoyTW <152999162+TinyjoyTW@users.noreply.github.com>
2024-02-19 17:12:16 +0100

    REST api day 1

--- server/app.js
+++ server/app.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const morgan = require("morgan");
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
+const app = express();
 
+app.use(
+  cors({
+    // Add the URLs of allowed origins to this array
+    origin: ['http://localhost:5173'],
+  })
+);
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
-const app = express();
 
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+// Use the CORS middleware with options to allow requests
+// from specific IP addresses and domains.
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -28,6 +40,15 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
+
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
+
+app.get("/api/students", (req, res) => {
+  res.json(students);
+});
+
 
 
 // START SERVER
