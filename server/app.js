edc6a520f271ecdf6ac3f4d166e67985305de56d
Maria Fernanda <fernandaantunesw@gmail.com>
2024-02-26 16:02:59 +0000

    day1

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const students = require("./students.json");
+const cohort = require("./cohorts.json")
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -25,8 +27,17 @@ app.use(cookieParser());
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
+
 app.get("/docs", (req, res) => {
-  res.sendFile(__dirname + "/views/docs.html");
+res.sendFile(__dirname + "/views/docs.html")
+})
+
+app.get("/api/cohort", (req, res) => {
+  res.status(200).json(cohort); 
+});
+
+app.get("/api/students", (req, res) => {
+  res.status(200).json(students); 
 });
 
 
@@ -34,3 +45,4 @@ app.get("/docs", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
