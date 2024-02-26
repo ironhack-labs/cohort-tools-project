085e40ca72cd7b56b03b83bd96c52bc9dfa7ffbc
Pauline Cvt <paulinecvt2021@gmail.com>
2024-02-26 16:53:48 +0100

    installed CORS in app.js

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const PORT = 5005;
 const cohorts = require("../server/cohorts.json");
 const students = require("../server/students.json");
+const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -20,6 +21,13 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// CORS
+app.use(
+  cors ({
+    origin: "http://localhost:5005",
+  })
+);
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
