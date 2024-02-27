c864687a4c43544b6801066e2903c56c267d0977
“Filipe.Santiago” <thekyd42@gmail.com>
2024-02-27 17:59:05 +0000

    day2-18h00

---
+++ server/model/Student.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const studentsSchema = new Schema({
+    firstName: {type: String, required: true},
+    lastName:  {type: String, required: true}, 
+    email: {type: String, required: true, unique: true}, 
+    phone: {type: String, required: true}, 
+    linkedinUrl: {type: String, default: ""}, 
+    languages: {type: [String], enum: ["English", "Spanish", "French", "German", "Portuguese", "Dutch", "Other"]}, 
+    program: {type: String, enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]}, 
+    background: {type:String, default:''}, 
+    image: {type: String, default: "https://i.imgur.com/r8bo8u7.png" }, 
+    cohort: {type: Schema.Types.ObjectId, ref: "Cohort"}, 
+    projects: {type: Array} 
+  })
+   
+  const Student = mongoose.model("Student", studentsSchema);
+  module.exports = Student;
