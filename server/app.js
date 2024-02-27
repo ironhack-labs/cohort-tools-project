7e7ad017f5e0804b0e61bcba2da88122f192b193
Maria Fernanda <fernandaantunesw@gmail.com>
2024-02-27 16:51:10 +0000

    day2

--- server/app.js
+++ server/app.js
@@ -2,34 +2,60 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
-const students = require("./students.json");
-const cohorts = require("./cohorts.json");
 const cors = require("cors");
+const mongoose = require("mongoose");
+//import
+const Student = require("./models/Student.model.js");
+const Cohort = require("./models/Cohort.model.js")
+const app = express(); // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
 
-// STATIC DATA
-// Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
+
+  app.use(
+    cors({
+      origin: ['http://localhost:5173'], 
+    })
+  );
+  app.use(express.json());
+  app.use(morgan("dev"));
+  app.use(express.static("public"));
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
 
 
-// INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
-const app = express();
+app.get("/cohorts", (req, res) => {
+  Cohort.find({})
+  .then ((cohorts) => {
+    console.log("Retrieve cohorts ->", cohorts);
+    res.json(cohorts)
+  })
+  .catch((error) =>{
+    console.error("Error", error);
+    res.status(500).json({error: "Failed to retrieve cohorts"});
+  });
+})
 
+app.get("/students", (req, res) => {
+  Student.find({})
+  .then ((students) => {
+    console.log("Retrieve students ->", students);
+    res.json(students)
+  })
+  .catch((error) =>{
+    console.error("Error", error);
+    res.status(500).json({error: "Failed to retrieve students"});
+  });
+})
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
-app.use(
-  cors({
-    origin: ['http://localhost:5173'], 
-  })
-);
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
