470d86ebe6a722f87ab960ba85bbaf722081e20a
Stefan Kummerlöw <gummiz@gmx.net>
2024-02-27 15:47:33 +0100

    feat(app.js): add connection to MongoDB using mongoose to establish a connection to the database
    feat(Cohort.model.js): create a schema and model for the Cohort collection in the database to define the structure and properties of a cohort
    
    feat(Student.model.js): create a schema and model for the Student collection in the database to define the structure and properties of a student

--- server/app.js
+++ server/app.js
@@ -4,6 +4,14 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const PORT = 5005;
 
+// Connection to MongoDB with mongoose
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
+
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 
