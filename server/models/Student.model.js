6aae8aae76830481444ec73e4dd3f7d54cb74472
Stefan Kummerlöw <gummiz@gmx.net>
2024-02-28 17:33:58 +0100

    refactor(Student.model.js): improve code formatting and add missing reference to Cohort model in the cohort field

--- server/models/Student.model.js
+++ server/models/Student.model.js
@@ -5,17 +5,37 @@ const Schema = mongoose.Schema;
 // Schema - describes and enforces the structure of the documents
 
 const studentSchema = new Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true },
-    linkedinUrl: { type: String, default: "" },
-    languages: {type: String, enum: ["English", "Spanish", "French", "German", "Portuguese", "Dutch", "Other"]},
-    program: {type: String, enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]},
-    background: { type: Boolean, default: "" },
-    image: { type: String, default: "https://i.imgur.com/r8bo8u7.png" },
-    cohort:  Schema.Types.ObjectId, //`ObjectId` - Reference *_id* of the cohort the student belongs to. 
-    projects: { type: Array },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  linkedinUrl: { type: String, default: "" },
+  languages: {
+    type: String,
+    enum: [
+      "English",
+      "Spanish",
+      "French",
+      "German",
+      "Portuguese",
+      "Dutch",
+      "Other",
+    ],
+  },
+  program: {
+    type: String,
+    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
+  },
+  background: { type: Boolean, default: "" },
+  image: {
+    type: String,
+    default: "https://i.imgur.com/r8bo8u7.png",
+  },
+  cohort: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Cohort",
+  }, 
+  projects: { type: Array },
 });
 
 // CREATE MODEL
@@ -24,5 +44,3 @@ const Student = mongoose.model("Student", studentSchema);
 
 // EXPORT THE MODEL
 module.exports = Student;
-
-
