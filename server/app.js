10e06d61cdfef24f32d091918db2e3cc5d8c2e6c
arisar86 <ari.sariannidis@gmail.com>
2024-02-28 18:03:31 +0100

    routes added

--- server/app.js
+++ server/app.js
@@ -60,7 +60,7 @@ app.get("/docs", (req, res) => {
 //   res.json(students);
 // });
 
-app.get("/students", (req, res) => {
+app.get("/api/students", (req, res) => {
   Student.find({})
     .then((students) => {
       console.log("Retrieved students ->", students);
@@ -72,7 +72,7 @@ app.get("/students", (req, res) => {
     });
 });
 
-app.get("/cohorts", (req, res) => {
+app.get("/api/cohorts", (req, res) => {
   Cohort.find({})
     .then((cohorts) => {
       console.log("Retrieved cohorts ->", cohorts);
@@ -81,6 +81,134 @@ app.get("/cohorts", (req, res) => {
     .catch((error) => {
       console.error("Error while retrieving cohorts ->", error);
       res.status(500).json({ error: "Failed to retrieve cohorts" });
+    });
+});
+
+app.post("/api/students", (req, res) => {
+  Student.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    phone: req.body.phone,
+    linkedinUrl: req.body.linkedinUrl,
+    languages: req.body.languages,
+    program: req.body.program,
+    background: req.body.background,
+    image: req.body.image,
+    cohort: req.body.cohort,
+    projects: req.body.projects,
+  })
+    .then((createdStudent) => {
+      res.status(201).json(createdStudent);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error creating new Student" });
+    });
+});
+
+app.get("/api/students", (req, res) => {
+  Student.find()
+    .then((allStudents) => {
+      res.status(200).json(allStudents);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error getting all Students" });
+    });
+});
+
+app.get("/api/students/cohort/:cohortId", (req, res) => {});
+
+app.get("/api/students/:studentId", (req, res) => {
+  Student.findById(req.params.studentId)
+    .then((student) => {
+      res.status(200).json(student);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error getting single Student" });
+    });
+});
+
+app.put("/api/students/:studentId", (req, res) => {
+  Student.findByIdAndUpdate(req.params.studentId, req.body, { new: true })
+    .then((updatedStudent) => {
+      res.status(200).json(updatedStudent);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error updating Student" });
+    });
+});
+
+app.delete("/api/students/:studentId", (req, res) => {
+  Student.findByIdAndDelete(req.params.studentId)
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error deleting Student" });
+    });
+});
+
+app.post("/api/cohorts", (req, res) => {
+  Cohort.create({
+    cohortSlug: req.body.cohortSlug,
+    cohortName: req.body.cohortName,
+    program: req.body.program,
+    format: req.body.format,
+    campus: req.body.campus,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    inProgress: req.body.inProgress,
+    programManager: req.body.programManager,
+    leadTeacher: req.body.leadTeacher,
+    totalHours: req.body.totalHours,
+  })
+    .then((createdCohort) => {
+      res.status(201).json(createdCohort);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error creating new Cohort" });
+    });
+});
+
+app.get("/api/cohorts", (req, res) => {
+  Student.find()
+    .then((allCohorts) => {
+      res.status(200).json(allCohorts);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error getting all Cohorts" });
+    });
+});
+
+app.get("/api/cohorts/:cohortId", (req, res) => {});
+
+app.get("/api/cohorts/:cohortId", (req, res) => {
+  Cohort.findById(req.params.cohortId)
+    .then((cohort) => {
+      res.status(200).json(cohort);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error getting single Cohort" });
+    });
+});
+
+app.put("/api/cohorts/:cohortId", (req, res) => {
+  Cohort.findByIdAndUpdate(req.params.cohortId, req.body, { new: true })
+    .then((updatedCohort) => {
+      res.status(200).json(updatedCohort);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error updating Cohort" });
+    });
+});
+
+app.delete("/api/cohorts/:cohortId", (req, res) => {
+  Cohort.findByIdAndDelete(req.params.cohortId)
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error deleting Cohort" });
     });
 });
 
