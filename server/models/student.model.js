e4cdd96e09b2f85ed90179ae3d042a90efc06b9e
MateusCTO <farias.mateus@gmail.com>
2024-02-28 09:15:50 +0000

    relationships

--- server/models/student.model.js
+++ server/models/student.model.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-import Cohort from "./cohort.model"
 
 const studentSchema = new Schema({
   fistName: { type: String, required: true },
@@ -8,14 +7,13 @@ const studentSchema = new Schema({
   email: {
     type: String, required: true, unique: true
   },
-
   phone: { type: String, required: true },
   linkedinUrl: { type: String, default: "" },
   languages: { type: [], enum: ["English", "Spanish", "French", "German", "Portuguese", "Dutch", "Other"] },
   program: { type: String, enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"] },
   background: { type: String, default: "" },
   image: { type: String, default: "https://i.imgur.com/r8bo8u7.png" },
-  cohort: Cohort._id,
+  cohort: { type: mongoose.Schema.Types.ObjectId, ref: "Cohort" },
   projects: []
 })
 
