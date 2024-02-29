996e2c008499e1fe970a9e30c9f0031a00b73597
Maria Fernanda <fernandaantunesw@gmail.com>
2024-02-29 14:00:59 +0000

    day3

--- server/routes/cohort.routes.js
+++ server/routes/cohort.routes.js
@@ -46,8 +46,36 @@ router.put("/cohort/:id", async (req, res) => {
     try {
       /* Destructure the id via router params */
       const { id } = req.params;
+      const {
+        cohortSlug, 
+        cohortName, 
+        program,
+        format,
+        campus,
+        startDate,
+        endDate,
+        inProgress, 
+        programManager, 
+        leadTeacher,
+        totalHours} = req.body
+
+        if(!cohortSlug|| !cohortName || !programManager || !leadTeacher){
+            return res.status(400).json({message: "Please fill all mandatory fields!"})
+          }
       /* Find the user via the id and send it back to the client */
-      const updateCohort = await Cohort.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      const updateCohort = await Cohort.findByIdAndUpdate(id, {
+        cohortSlug, 
+        cohortName, 
+        program,
+        format,
+        campus,
+        startDate,
+        endDate,
+        inProgress, 
+        programManager, 
+        leadTeacher,
+        totalHours
+      }, { new: true });
       res.status(200).json(updateCohort);
     } catch (error) {
         res.status(500).json({message: "Error while creating the Cohort"});
@@ -61,10 +89,10 @@ router.delete("/cohort/:id", async (req, res) => {
       /* Destructure the id via route params */
       const { id } = req.params;
       /* Find the user via the id and send it back to the client */
-      const deleteCohort = await Cohort.findByIdAndDelete(req.params.id);
-      res.status(204).json(deleteCohort);
+      await Cohort.findByIdAndDelete(id);
+      res.status(200).json("Cohort was deleted");
     } catch (error) {
-        res.status(500).json({message: "Error while creating the Cohort"});
+        res.status(500).json({message: "Error while deleting the Cohort"});
     }
   })
 
