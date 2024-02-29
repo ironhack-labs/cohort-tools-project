63ba569acbb284752f39e0e329846d37ac0cdf6f
EazyErik <erik_busch@gmx.de>
2024-02-29 08:50:01 +0100

    fix route

--- client/src/pages/StudentListPage.jsx
+++ client/src/pages/StudentListPage.jsx
@@ -11,7 +11,7 @@ function StudentListPage() {
 
   useEffect(() => {
     axios
-      .get(`${API_URL}/students?$`)
+      .get(`${API_URL}/api/students?$`)
       .then((response) => {
         setStudents(response.data);
       })
