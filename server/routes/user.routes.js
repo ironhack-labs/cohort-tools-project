93ee195b948df5ffab05c3d4d5137979de2ce923
SantosMatheusSilva <mathsantos52@gmail.com>
2024-03-01 17:00:12 +0000

    done

---
+++ server/routes/user.routes.js
@@ -0,0 +1,2 @@
+//Use the authentication middleware to protect the GET /api/users/:id route. 
+//The route should be protected using the authentication middleware and accessible only to authenticated users.
