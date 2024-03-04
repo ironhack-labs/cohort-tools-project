1b3fca8e094d13ea5f15acacc5c7d1f4f15d1658
Josep Climent <josepcliment92@gmail.com>
2024-03-04 15:25:10 +0100

    finalizar tareas dia 1

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 
+const cors = require("cors")
+
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 const importedStudents = require("./students.json")
@@ -17,7 +19,9 @@ const app = express();
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
-// ...
+app.use(cors({
+  origin: ["http://localhost:5005"]
+}))
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
