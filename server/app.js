9f2d826e9beb4a11276667f971d4a07a35f93955
Josep Climent <josepcliment92@gmail.com>
2024-03-04 15:08:49 +0100

    tareas dia 1

--- server/app.js
+++ server/app.js
@@ -5,7 +5,10 @@ const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
+const importedStudents = require("./students.json")
+//console.log(importedStudents)
+const importedCohorts = require("./cohorts.json")
+//console.log(importedCohorts)
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
@@ -28,6 +31,14 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
+
+app.get("/api/cohorts", (req, res) => {
+  res.json(importedCohorts)
+})
+
+app.get("/api/students", (req, res) => {
+  res.json(importedStudents)
+})
 
 
 // START SERVER
