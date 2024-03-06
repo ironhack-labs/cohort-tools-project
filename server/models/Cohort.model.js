b6d871c0efe388475669b80576600cfd9e6346b0
Francisco Estepa Guerra <estepaguerrafrancisco@gmail.com>
2024-03-06 18:25:52 +0100

    dia 3

--- server/models/Cohort.model.js
+++ server/models/Cohort.model.js
@@ -2,17 +2,50 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const cohortSchema = new Schema ({
-    cohortSlug: {type: String, unique: true},
-    cohortName: String,
-    program: {type: String, enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]},
-    format: {type: String, enum: ["Full Time", "Part Time"]},
-    campus: {type: String, enum: ["Madrid", "Barcelona", "Miami", "Paris", "Berlin", "Amsterdam", "Lisbon", "Remote"]},
-    startDate: {type: Date, default: Date.now},
-    endDate: Date,
-    inProgress: {type: Boolean, default: false},
-    programManager: String,
-    leadTeacher: String,
-    totalHours: {type: Number, default: 360}
+    cohortSlug: {
+        type: String, 
+        unique: true,
+        required: true
+    },
+    cohortName: {
+        type: String, 
+        required: true
+    },
+    program: {
+        type: String, 
+        enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]
+    },
+    format: {
+        type: String, 
+        enum: ["Full Time", "Part Time"]
+    },
+    campus: {
+        type: String, 
+        enum: ["Madrid", "Barcelona", "Miami", "Paris", "Berlin", "Amsterdam", "Lisbon", "Remote"]
+    },
+    startDate: {
+        type: Date, 
+        default: Date.now
+    },
+    endDate: {
+        type: Date
+    },
+    inProgress: {
+        type: Boolean, 
+        default: false
+    },
+    programManager: {
+        type: String,
+        required: true
+    },
+    leadTeacher: {
+        type: String,
+        required: true
+    },
+    totalHours: {
+        type: Number, 
+        default: 360
+    }
 })
 
 const Cohort = mongoose.model("Cohort", cohortSchema);
--- server/models/Student.model.js
+++ server/models/Student.model.js
@@ -3,17 +3,49 @@ const Schema = mongoose.Schema;
 
 
 const studentSchema = new Schema ({
-    firstName: String,
-    lastName: String,
-    email: {type: String, unique: true },
-    phone: String,
-    linkedinUrl: {type: String, default: ""},
-    languages: {type: String, enum: ["English", "Spanish", "French", "German", "Portuguese", "Dutch", "Other"]},
-    program: {type: String, enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]},
-    background: {type: String, default: ""},
-    image: {type: String, default: "https://i.imgur.com/r8bo8u7.png" },
-    cohort: Object,
-    projects: Array,  // OJO
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String, 
+        unique: true 
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    linkedinUrl: {
+        type: String, 
+        default: ""
+    },
+    languages: {
+        type: [String],
+        enum: ["English", "Spanish", "French", "German", "Portuguese", "Dutch", "Other"]
+    },
+    program: {
+        type: String, 
+        enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]
+    },
+    background: {
+        type: String, 
+        default: ""
+    },
+    image: {
+        type: String, 
+        default: "https://i.imgur.com/r8bo8u7.png" 
+    },
+    cohort: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Cohort"
+    },
+    projects: {
+        type: [String]
+    },  
 })
 
 const Student = mongoose.model("Student", studentSchema);
