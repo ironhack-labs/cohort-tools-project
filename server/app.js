774141468b7aaad6667ef32a2a375ef41c46cc90
Adrian Doonan <adriandoonan@gmail.com>
2024-03-25 16:58:01 +0100

    add cors

--- server/app.js
+++ server/app.js
@@ -1,36 +1,46 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const referrerPolicy = require("referrer-policy");
 const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+app.use(referrerPolicy({ policy: "strict-origin" }));
+app.use(
+	cors({
+		origin: ["http://localhost:5173", "http://example.com"], // Add the URLs of allowed origins to this array
+	}),
+);
+
+app.use((req, res, next) => {
+	res.setHeader("Referrer-Policy", "strict-origin");
+	next();
+});
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
 app.get("/docs", (req, res) => {
-  res.sendFile(__dirname + "/views/docs.html");
+	res.sendFile(`${__dirname}/views/docs.html`);
 });
-
 
 // START SERVER
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port ${PORT}`);
 });
