e2c178265ef2bd52ee523b4bed3b30062c660d72
Piero Schenato <piero.schenato@gmail.com>
2024-03-25 16:54:26 +0100

    day 1 done

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
+
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-
-
+const cohorts = require ("./cohorts.json");
+const students = require ("./students.json");
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
@@ -29,6 +30,13 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get ('/api/cohorts', (req,res) =>{
+  res.json(cohorts);
+  });
+
+  app.get ('/api/students', (req,res) =>{
+    res.json(students);
+    });
 
 // START SERVER
 app.listen(PORT, () => {
