0f3acde35de6f848f297ac2dd4b2047180f83737
Memoryman2022 <rlacannon@gmail.com>
2024-03-26 17:30:05 +0100

    something

--- server/app.js
+++ server/app.js
@@ -20,17 +20,17 @@ const app = express();
 const mongoose = require("mongoose");
 
 mongoose
-	.connect("mongodb://127.0.0.1:27017/cohort-tools-api")
-	.then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
-	.catch((err) => console.error("Error connecting to MongoDB", err));
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
 app.use(
-	cors({
-		origin: ["http://localhost:5173", "http://example.com"], // Add the URLs of allowed origins to this array
-	}),
+  cors({
+    origin: ["http://localhost:5173", "http://example.com"], // Add the URLs of allowed origins to this array
+  })
 );
 app.use(express.json());
 app.use(morgan("dev"));
@@ -42,42 +42,34 @@ app.use(cookieParser());
 // Devs Team - Start working on the routes here:
 // ...
 app.get("/docs", (req, res) => {
-	res.sendFile(__dirname + "/views/docs.html");
-});
-
-app.get("/api/cohorts", (req, res) => {
-	res.json(cohorts);
+  res.sendFile(__dirname + "/views/docs.html");
 });
 
 app.get("/api/students", (req, res) => {
-	res.json(students);
+  Student.find({})
+    .then((students) => {
+      console.log("found students ->", students);
+      res.json(students);
+    })
+    .catch((error) => {
+      console.error("Error finding students ->", error);
+      res.status(500).send({ error: "Lost students" });
+    });
 });
 
-app.get("/students", (req, res) => {
-	Student.find({})
-		.then((students) => {
-			console.log("found students ->", students);
-			res.json(students);
-		})
-		.catch((error) => {
-			console.error("Error finding students ->", error);
-			res.status(500).send({ error: "Lost students" });
-		});
-});
-
-app.get("/cohorts", (req, res) => {
-	Cohort.find({})
-		.then((cohorts) => {
-			console.log("found cohorts ->", cohorts);
-			res.json(cohorts);
-		})
-		.catch((error) => {
-			console.error("Error finding cohorts ->", error);
-			res.status(500).send({ error: "Lost cohorts" });
-		});
+app.get("/api/cohorts", (req, res) => {
+  Cohort.find({})
+    .then((cohorts) => {
+      console.log("found cohorts ->", cohorts);
+      res.json(cohorts);
+    })
+    .catch((error) => {
+      console.error("Error finding cohorts ->", error);
+      res.status(500).send({ error: "Lost cohorts" });
+    });
 });
 
 // START SERVER
 app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
